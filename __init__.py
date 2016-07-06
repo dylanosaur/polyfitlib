ef0dfc22c87bdd4b029e7522a6284c3224bbbd5b
dylanosaur <dadams7@wisc.edu>
2016-07-06 11:23:25 -0500

    commented out import statements in __init__ file

--- __init__.py
+++ __init__.py
@@ -1,29 +1,29 @@
 try:
-    from ACPhotons import calcACPhotons
-    from AmpOffset import calcAmpOffset
-    from _CharPulseBeginAndEnd import _calcCharPulseBeginAndEnd
-    from FilterChans import filterChans
-    from _GetACDataAndCharPulse import _getACDataAndCharPulse
-    from _GetDCDataAndCharPulse import _getDCDataAndCharPulse
-    from GetLaserFireTimes import getLaserFireTimes
-    from LambdaArray import calcLambdaArray
-    from NeTeInitVals import calcTeNeInitVals
-    from _NeTeProbability import _calcNeTeProbability
-    from NeTeWithErrors import calcNeTeValuesWithErrors
-    from _N__model import _N_model
-    from NumPhotons import calcNumPhotons
-    from _PolyPulseFitFn import _polyPulseFitFn
-    from SaveToMDSPlus import saveToMDSplus
-    from ScatteringAngle import calcScatteringAngle
-    from T0 import calc_t0
-    from TransMask import calcTransMask
-    from VoltageFromRawData import calcVoltageFromRawData
-    from WriteOutputFile import writeOutputFile
+    #from ACPhotons import calcACPhotons
+    #from AmpOffset import calcAmpOffset
+    #from _CharPulseBeginAndEnd import _calcCharPulseBeginAndEnd
+    #from FilterChans import filterChans
+    #from _GetACDataAndCharPulse import _getACDataAndCharPulse
+    #from _GetDCDataAndCharPulse import _getDCDataAndCharPulse
+    #from GetLaserFireTimes import getLaserFireTimes
+    #from LambdaArray import calcLambdaArray
+    #from NeTeInitVals import calcTeNeInitVals
+    #from _NeTeProbability import _calcNeTeProbability
+    #from NeTeWithErrors import calcNeTeValuesWithErrors
+    #from _N__model import _N_model
+    #from NumPhotons import calcNumPhotons
+    #from _PolyPulseFitFn import _polyPulseFitFn
+    #from SaveToMDSPlus import saveToMDSplus
+    #from ScatteringAngle import calcScatteringAngle
+    #from T0 import calc_t0
+    #from TransMask import calcTransMask
+    #from VoltageFromRawData import calcVoltageFromRawData
+    #from WriteOutputFile import writeOutputFile
 except Exception, ex:
     print 'failed to import all functions, error is: ', ex
 
 try:
-    from MostProbable_NeTe import calcMostProbable_neTe
+    #from MostProbable_NeTe import calcMostProbable_neTe
 except Exception, ex:
     print 'failed to import all functions, error is: ', ex
 
@@ -37,9 +37,9 @@ __all__ = ['calcACPhotons', 'calcAmpOffset', '_calcCharPulseBeginAndEnd',
            'writeOutputFile' ]
 
 try:
-    from FitPolySeg import fitPolySeg
-    from FitPSstar import fitPSstar
-    from FitWithWarnings import fitWithWarnings
+    #from FitPolySeg import fitPolySeg
+    #from FitPSstar import fitPSstar
+    #from FitWithWarnings import fitWithWarnings
 except Exception, ex:
     print 'fitOps module not loaded correctly, error: ', ex
 
@@ -47,13 +47,13 @@ __all__ += ['fitPolySeg', 'fitPSstar', 'fitWithWarnings' ]
 
 
 try:
-    from Calibration import PolyCalibration
+    from calibration import PolyCalibration
     from Data import PolySegData, Data
-    from Spectral_weave import cold2o_Spec, selden_Spec, selden_old
-    from Spectral_weave import calcModelProbability, compareGrid,  specPhotons
-    from Util import getShotsForDate, getSaveFilePath, getDataFilePath
-    from Util import getCalibrationFilePath, getMdsServer
-    from spec_test_compile import test_compile
+    from spectral_weave import cold2o_Spec, selden_Spec, selden_old
+    from spectral_weave import calcModelProbability, compareGrid,  specPhotons
+    from util import getShotsForDate, getSaveFilePath, getDataFilePath
+    from util import getCalibrationFilePath, getMdsServer
+    #from spec_test_compile import test_compile
 except Exception, ex:
     print 'auxOps module not loaded correctly, error: ', ex
 
@@ -61,7 +61,7 @@ __all__ += ['cold2o_Spec', 'selden_Spec', 'selden_old',
            'calcModelProbability', 'test_compile', 'compareGrid',
            'specPhotons', 'PolyCalibration', 'Data', 'PolySegData']
     
-from globals import init_globals
+#from globals import init_globals
 
 __all__ += ["init_globals"]
 
