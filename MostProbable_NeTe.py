fe1f6d4ef66e809e7e1475f67675703e9e7d2080
dylanosaur <dadams7@wisc.edu>
2016-07-18 13:42:43 -0500

    added remove zeros function to avoid shape mismatch when comparing model to calibration data

--- MostProbable_NeTe.py
+++ MostProbable_NeTe.py
@@ -7,9 +7,7 @@ def calcMostProbable_neTe(ps, specFlag = "tsc"):
 
     def __calcNeTeProbability(x, ps, specFlag):
         try:
-            print 'before calcProb'
             return_val = _calcNeTeProbability(x[0], x[1], ps, specFlag)
-            print 'after calcProb'
             return return_val
         except Exception, ex:
             print 'unknown error', ex
@@ -19,6 +17,21 @@ def calcMostProbable_neTe(ps, specFlag = "tsc"):
         args=(ps, specFlag, ), disp=0, full_output=1)
 
     predicted=_N_model(xopt[0],xopt[1],ps,'tsc')
+
+    # identified most of 100 errors related to shape mismatch here
+    # added these 4 lines + function at end as possible bug fix
+    if len(predicted) != len(ps.scatPhotonsDC):
+        ps.scatPhotonsDC = remove_zeros(ps.scatPhotonsDC, ps.chanFlagDC)
+        ps.bgPhotons = remove_zeros(ps.bgPhotons, ps.chanFlagDC)
+        ps.calib.APDfqeDC = remove_zeros(ps.calib.APDfqeDC, ps.chanFlagDC)
+
+    # new error message defined here for diagnosis
+    try: ps.chiRedMin=sum((predicted - ps.scatPhotonsDC)**2/((predicted + ps.bgPhotons) * ps.calib.APDfqeDC))/len(ps.scatPhotonsDC)
+    except Exception, ex:
+        ps.errNum = 900
+        ps.errMsg = ex
+
+
     ps.chiRedMin=sum((predicted - ps.scatPhotonsDC)**2/((predicted + ps.bgPhotons) * ps.calib.APDfqeDC))/len(ps.scatPhotonsDC)
     ps.residual=(predicted - ps.scatPhotonsDC) #calculates a residual
     ps.weight = 1/((predicted + ps.bgPhotons) * ps.calib.APDfqeDC)
@@ -29,3 +42,24 @@ def calcMostProbable_neTe(ps, specFlag = "tsc"):
 
     ps.ne1 = xopt[0]
     ps.Te1 = xopt[1]
+
+# function takes 2 1-D arrays of size n
+# flag array is an array of 1's and 0's
+# return shortened array with entries corresponding
+# to 0's in flag array removed
+from numpy import ndarray
+def remove_zeros(array, flag_array):
+    n = len(array)
+    count = 0
+    for i in xrange(0, n):
+        if flag_array[i] != 0: count += 1
+        else: pass
+
+    new = ndarray([count])
+    index = 0
+    for i in xrange(0, n):
+        if flag_array[i] != 0: new[index] = array[i]
+        else: index -= 1
+        index += 1
+
+    return new
